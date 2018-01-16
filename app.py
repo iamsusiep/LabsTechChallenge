5032d00919acc69452759d211c7ec502f03b9045
iamsusiep <iamsusie@gmail.com>
2018-01-16 10:34:52 -0500

    created a page

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/sp3581')
+def sp():
+    return render_template('webpage.html')
 
 if __name__ == '__main__':
     app.run()
